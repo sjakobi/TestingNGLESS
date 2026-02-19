4e2d0e7de57184796473cd3140cafd83f785719d
Simon Jakobi <simon.jakobi@gmail.com>
2026-02-19 16:24:41 +0100

    Inline Data.Conduit.Binary and remove conduit-extra

--- vendor/conduit-extra-1.3.8/Data/Conduit/Filesystem.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE RankNTypes #-}
--- | /NOTE/ It is recommended to start using "Data.Conduit.Combinators" instead
--- of this module.
-module Data.Conduit.Filesystem
-    ( CC.sourceDirectory
-    , CC.sourceDirectoryDeep
-    ) where
-
-import qualified Data.Conduit.Combinators as CC
--- vendor/conduit-extra-1.3.8/README.md
+++
@@ -1,5 +0,0 @@
-## conduit-extra
-
-For more information about conduit in general, and how this package in
-particular fits into the ecosystem, see [the conduit
-homepage](https://github.com/snoyberg/conduit#readme).
